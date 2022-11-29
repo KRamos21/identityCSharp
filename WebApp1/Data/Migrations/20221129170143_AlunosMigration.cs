cb47ce4c77ff79d814b5ec8d2cc61a19877110e4
Kauê Ramos Raimundo <kaue.ramos.krr@gmail.com>
2022-11-29 14:48:58 -0300

    Initial commit

---
+++ WebApp1/Data/Migrations/20221129170143_AlunosMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp1.Data.Migrations
+{
+    public partial class AlunosMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aluno",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeAluno = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailAluno = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aluno", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aluno");
+        }
+    }
+}
---
+++ WebApp1/Models/Aluno.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp1.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        public string NomeAluno { get; set; }
+
+        public string EmailAluno { get; set; }
+    }
+}
